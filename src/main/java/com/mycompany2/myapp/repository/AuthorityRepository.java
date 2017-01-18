5b400851a2998ed05982bc5cd2c6605935005ee5
김용휘(YongHwee Kim)/Text Analysis Service팀/SKP <yonghwee.kim@sk.com>
2017-01-18 10:36:42 +0900

    first commit

---
+++ src/main/java/com/mycompany2/myapp/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.mycompany2.myapp.repository;
+
+import com.mycompany2.myapp.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/mycompany2/myapp/web/filter/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Servlet filters.
+ */
+package com.mycompany2.myapp.web.filter;
